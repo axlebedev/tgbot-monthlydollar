a052f40c520a200373a8cf27fdfdf1ae3601e5a1
Alex <alexey.broadcast@gmail.com>
2020-03-23 23:25:08 +0300

    drawRatesChart: draw finish value at axis

--- src/drawImage/drawRatesChart.js
+++ src/drawImage/drawRatesChart.js
@@ -1,4 +1,6 @@
 import sum from 'lodash/sum'
+import last from 'lodash/last'
+import round from 'lodash/round'
 
 import { drawCircle } from './drawUtils'
 import {
@@ -6,8 +8,11 @@ import {
   avgChartColor,
 } from './colors'
 
+const fontSize = 15
+
 export const drawRatesChart = async ({ ctx, rates, xScale, yScale }) => {
   ctx.strokeStyle = ratesChartColor
+  ctx.fillStyle = ratesChartColor
   ctx.lineWidth = 3
   ctx.setLineDash([])
   ctx.beginPath()
@@ -17,6 +22,19 @@ export const drawRatesChart = async ({ ctx, rates, xScale, yScale }) => {
     drawCircle(ctx, xScale(rates[i].date), yScale(rates[i].value))
   }
   ctx.stroke()
+
+  const [, right] = xScale.range()
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+  ctx.fillRect(
+    right + fontSize / 3,
+    yScale(last(rates).value) - fontSize,
+    fontSize * 3,
+    fontSize,
+  )
+  ctx.fillStyle = ratesChartColor
+  ctx.textAlign = 'left'
+  ctx.font = `bold ${fontSize}px Impact`
+  ctx.fillText(` ${round(last(rates).value, 2)}`, right, yScale(last(rates).value))
 }
 
 const getAvgRates = (rates) => {
@@ -35,6 +53,7 @@ const getAvgRates = (rates) => {
 export const drawAvgRatesChart = async ({ ctx, rates, xScale, yScale }) => {
   const avgRates = getAvgRates(rates)
 
+  ctx.fillStyle = avgChartColor
   ctx.strokeStyle = avgChartColor
   ctx.lineWidth = 3
   ctx.setLineDash([])
@@ -45,4 +64,17 @@ export const drawAvgRatesChart = async ({ ctx, rates, xScale, yScale }) => {
     drawCircle(ctx, xScale(avgRates[i].date), yScale(avgRates[i].value))
   }
   ctx.stroke()
+
+  const [, right] = xScale.range()
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.5)'
+  ctx.fillRect(
+    right + fontSize / 3,
+    yScale(last(avgRates).value) - fontSize,
+    fontSize * 3,
+    fontSize,
+  )
+  ctx.fillStyle = avgChartColor
+  ctx.textAlign = 'left'
+  ctx.font = `bold ${fontSize}px Impact`
+  ctx.fillText(` ${round(last(avgRates).value, 2)}`, right, yScale(last(avgRates).value))
 }
